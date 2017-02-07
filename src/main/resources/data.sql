cbb2bbcb1dbd568701ad0c8dde9a009b14534bd5
Roman Gemini <roman@homefs.biz>
2017-02-07 09:50:36 +0200

    Remove dbunit because we'll use H2 data.sql.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,9 @@
 INSERT INTO "USERS" ("EMAIL", "PASSWORD", "NAME", "ROLE", "STATUS")
 VALUES
   ('foo@bar.com', 'abc', 'Foo Bar', 'USER', 'ACTIVE'),
-  ('baz@bar.com', '123', 'Baz Bar', 'ADMIN', 'ACTIVE');
+  ('baz@bar.com', '123', 'Baz Bar', 'ADMIN', 'ACTIVE'),
+  ('sam@foo.com', 'GkyxE8Fts6aHBQxs', 'Sam Fisher', 'ADMIN', 'ACTIVE'),
+  ('mat@foo.com', 'LHzxdqkRPGKPdcY6', 'Mathew Ryan', 'USER', 'ACTIVE'),
+  ('bob@foo.com', 'S4BcdadcKNc75L68', 'Bob Dylan', 'USER', 'INACTIVE'),
+  ('jack@foo.com', 'WpnMXCqcWHn4KLzn', 'Jack Pack','USER','BANNED'),
+  ('alan@brew.com', 'r3Hgpp5xLyRnW95n', 'Alan Away', 'USER', 'SUSPENDED');
--- src/test/kotlin/com/radioteria/controllers/AbstractControllerTest.kt
+++ src/test/kotlin/com/radioteria/controllers/AbstractControllerTest.kt
@@ -1,24 +1,15 @@
 package com.radioteria.controllers
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener
-import com.github.springtestdbunit.annotation.DatabaseSetup
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.TestExecutionListeners
-import org.springframework.test.context.TestExecutionListeners.*
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestExecutionListeners(
-        DbUnitTestExecutionListener::class,
-        mergeMode = MergeMode.MERGE_WITH_DEFAULTS
-)
-@DatabaseSetup("classpath:datasets/user-table.xml")
 abstract class AbstractControllerTest {
     @Autowired lateinit var mvc: MockMvc
 }
