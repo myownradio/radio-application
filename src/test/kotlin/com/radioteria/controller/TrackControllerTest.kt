77bd49d6fc2ea2c8f2f2f60661414edeec47de71
Roman Gemini <roman@homefs.biz>
2017-02-10 15:44:49 +0200

    Add Track crud operations.

---
+++ src/test/kotlin/com/radioteria/controller/TrackControllerTest.kt
@@ -0,0 +1,14 @@
+package com.radioteria.controller
+
+import org.junit.Test
+
+class TrackControllerTest : AbstractControllerTest() {
+    companion object {
+        const val API_TRACKS_ENDPOINT = "/api/channel/1/tracks"
+    }
+
+    @Test
+    fun createWhenAuthenticated() {
+
+    }
+}
