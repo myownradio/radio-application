644882ae516f505fc07868006052d55cccaac156
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-03-14 19:26:05 +0200

    Remove bower_components

--- src/main/frontend/webpack.config.js
+++ src/main/frontend/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
+                exclude: /(node_modules)/,
                 loader: 'babel-loader'
             },
         ]
